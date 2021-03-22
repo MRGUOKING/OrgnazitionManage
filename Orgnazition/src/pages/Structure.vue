c6578910c0d74e2e9f95bfba1b63fe1411cfd2f8
MRGUOKING <420919469@qq.com>
2021-03-23 01:21:48 +0800

    登录，注册页面，管理人员界面

--- Orgnazition/src/pages/Structure.vue
+++ Orgnazition/src/pages/Structure.vue
@@ -27,7 +27,53 @@
 
 <!--    右侧详情-->
     <article class="structure-right">
-      <section class="right-top"></section>
+<!--      右上-->
+      <section class="right-top">
+        <div class="button-container">
+          <button>新增员工</button>
+          <button>邀请员工</button>
+        </div>
+        <div class="search">
+          <input type="text" placeholder="搜索...">
+          <button class="iconfont">&#xe6e6</button>
+        </div>
+      </section>
+<!--      右下-->
+      <section class="right-bottom">
+        <table style="width: 100%">
+          <thead style="width: 100%">
+          <tr class="list-head">
+            <th>姓名</th>
+            <th>性别</th>
+            <th>部门</th>
+            <th>职务</th>
+            <th>账号</th>
+            <th>邮箱</th>
+            <th>手机</th>
+            <th>操作</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr class="list-head">
+            <th>郭江富</th>
+            <th>男</th>
+            <th>销售部</th>
+            <th>部员</th>
+            <th>12345</th>
+            <th>-</th>
+            <th>123434343</th>
+            <th>
+              <div>
+                <button>编辑</button>
+                <button>停用</button>
+                <button>冻结</button>
+              </div>
+
+            </th>
+          </tr>
+          </tbody>
+        </table>
+      </section>
     </article>
   </div>
 </div>
@@ -60,7 +106,7 @@ export default {
 .structure-right{
   width: 80%;
   height: 100%;
-  background-color: purple;
+  background-color: #ffffff;
   display: flex;
   flex-direction: column;
   justify-content: start;
@@ -127,5 +173,85 @@ export default {
   width: 100%;
   height: 50px;
   background-color: pink;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+}
+.right-top .button-container{
+  width: 50%;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  margin-left: 30px;
+}
+.button-container button{
+  /*margin-top: 10px;*/
+  margin-right: 20px;
+  width: 100px;
+  height: 30px;
+  outline: none;
+  border-radius: 5px;
+  border: 1px solid #7e8c8d;
+}
+
+.search{
+  display: flex;
+  justify-content: center;
+  margin-left: 20px;
+}
+.search input{
+  width: 200px;
+  height: 30px;
+  text-indent: 10px;
+  border-radius: 10px 0 0 10px;
+  outline: none;
+  margin-right: 0;
+
+}
+.right-bottom{
+  width: 100%;
+}
+.search button{
+  border-radius: 0px 5px 5px 0px;
+  float: right;
+  height: 30px;
+  border: none;
+  outline: none;
+  background-color: #4e6ef2;
+  cursor: pointer;
+}
+.search button:hover{
+  background-color: #4662d9;
+}
+.list-head{
+  padding: 0 20px;
+  /*width: 800px;*/
+  width: 1220px;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.list-head th{
+  flex: 1;
+}
+tbody{
+
+}
+tr{
+  box-sizing: border-box;
+  padding: 5px;
+  border-bottom: 1px solid #e6e6e6;
+  color: #666666;
+}
+thead tr{
+  background-color: #fafafa;
+}
+th button{
+  background-color: rgba(0,0,0,0);
+  outline: none;
+  border: none;
+  color: #00a8ff;
+  cursor: pointer;
 }
 </style>
